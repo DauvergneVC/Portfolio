603b5f56d1a19cdf164d654e964ccf08d3e40710
Vicente Dauvergne <vicente.dauvergne01@gmail.com>
2025-07-03 11:29:18 -0400

    React intregation and 404 page

---
+++ Client/src/layouts/NavBar.jsx
@@ -0,0 +1,18 @@
+// import { Link } from 'react-router-dom';
+
+function NavBar() {
+    return (
+    <header>
+    <h1>Vicente Dauvergne</h1>
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/projects">Projects</a></li>
+        </ul>
+    </nav>
+</header>
+)}
+
+
+export default NavBar;
