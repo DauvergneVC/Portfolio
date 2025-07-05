6e2a3dff872501f31db734a0ce2e6ea1b0650037
Vicente Dauvergne <vicente.dauvergne01@gmail.com>
2025-07-04 20:21:06 -0400

    Change Astro for only React

---
+++ Client/src/Layout/MainLayout.jsx
@@ -0,0 +1,33 @@
+
+export default function MainLayout () {
+    return(
+        <>
+            <header>
+                <NavBar/>
+            </header>
+
+            <article>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet consectetur ad minima qui consequuntur asperiores culpa,
+                    <br/> suscipit fugit facere architecto alias assumenda illum error quis voluptatem incidunt molestias ut? Doloremque!</p>
+            </article>
+
+            <footer>
+                <Footer/>
+            </footer>
+        </>
+    )
+}
+
+function NavBar () {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+function Footer () {
+    return(
+        <div></div>
+    )
+}
